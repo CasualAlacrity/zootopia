5809f4f012c3a42ab88902c5f96d86eaef283c41
Jeff Feenstra <feenstra32@gmail.com>
2026-03-19 17:25:50 +0100

    Step 1 commit reminder

---
+++ animals_web_generator.py
@@ -0,0 +1,30 @@
+import json
+from typing import List
+
+
+def run_app():
+    animals_data = load_data('data/animals_data.json')
+    print_animals(animals_data)
+
+
+def print_animals(animals_data: List[dict]):
+    for animal in animals_data:
+        animal_name = animal["name"]
+        animal_diet = animal["characteristics"].get("diet")
+        animal_location = animal["locations"][0]
+        animal_type = animal["characteristics"].get("type")
+
+        if animal_name: print(f"Name: {animal_name}")
+        if animal_diet: print(f"Diet: {animal_diet}")
+        if animal_location: print(f"Location: {animal_location}")
+        if animal_type: print(f"Type: {animal_type}")
+        print("") # New line for better readability
+
+def load_data(file_path):
+    """ Loads a JSON file """
+    with open(file_path, "r") as handle:
+        return json.load(handle)
+
+
+if __name__ == "__main__":
+    run_app()
