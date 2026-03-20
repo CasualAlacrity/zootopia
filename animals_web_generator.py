0db5f4419970929695c1233d38d8f683982e271c
Jeff Feenstra <feenstra32@gmail.com>
2026-03-20 10:28:43 +0100

    recreated the original html file. Added HTML trags to the parser

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -6,27 +6,34 @@ REPLACE_ANIMAL_INFO = "__REPLACE_ANIMALS_INFO__"
 
 def run_app():
     animals_data = load_data('data/animals_data.json')
-    html_template = load_template('templates/animals_template.html')
+    html_template = load_template('templates/original/animals_template.html')
 
-    html_template = html_template.replace(REPLACE_ANIMAL_INFO, print_animals(animals_data))
+    html_template = html_template.replace(REPLACE_ANIMAL_INFO, parse_animal_data_to_html(animals_data))
+
     with open('templates/animals_template.html', 'w') as f:
         f.write(html_template)
 
 
-def print_animals(animals_data: List[dict]) -> str:
-    animal_data = ""
+def parse_animal_data_to_html(animals_data: List[dict]) -> str:
+    animal_html_data = ""
+
     for animal in animals_data:
         animal_name = animal.get("name")
         animal_diet = animal.get("characteristics").get("diet")
         animal_location = animal.get("locations")[0]
         animal_type = animal.get("characteristics").get("type")
 
-        if animal_name: animal_data += f"Name: {animal_name}\n"
-        if animal_diet: animal_data += f"Diet: {animal_diet}\n"
-        if animal_location: animal_data += f"Location: {animal_location}\n"
-        if animal_type: animal_data += f"Type: {animal_type}\n"
+        animal_html_data += '<li class="cards__item">\n'  # Open the list item
 
-    return animal_data
+        # Add animal information to the HTML
+        if animal_name: animal_html_data += f"Name: {animal_name}<br/>\n"
+        if animal_diet: animal_html_data += f"Diet: {animal_diet}<br/>\n"
+        if animal_location: animal_html_data += f"Location: {animal_location}<br/>\n"
+        if animal_type: animal_html_data += f"Type: {animal_type}<br/>\n"
+
+        animal_html_data += '</li>\n'  # Close the list item
+
+    return animal_html_data
 
 
 def load_data(file_path):
